320852d61949f6a48fce45322b25bef5930e508f
clj500 <clj500@msstate.edu>
2022-04-30 18:57:33 -0500

    Added class files along with main.cpp

---
+++ Program Files/main.cpp
@@ -0,0 +1,116 @@
+﻿// MethodsProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace::std;
+
+#include "Tokenizer.h"
+#include "User.h"
+
+int main()
+{
+	bool runProg = true;
+	string user_input, command1;
+	vector<User> UserList;
+
+	Tokenizer tkn;
+
+	cout << "Welcome to 'The Book Store'" << endl << "Type help to pull up a list of commands" << endl;
+	cout << "-> LOGIN" << endl;
+	cout << "-> CREATE ACCOUNT" << endl;
+	cout << "-> EXIT PROGRAM" << endl;
+
+	while (runProg == true)
+	{
+		cout << ">> ";
+
+		//Get user input
+		getline(cin, user_input);
+		tkn.setString(user_input);
+
+		//Read user command
+		tkn.readWord(command1);
+
+		//User requests to create an account
+		if (command1 == "create")
+		{
+			string command2;
+
+			tkn.readWord(command2);
+
+			if (command2 == "account")
+			{
+				//Prepare user to set up their username
+				string username;
+				bool userWorked = false;
+				bool checkUserExists = false;
+
+				//Creates a logical error right now
+				while (userWorked == false)
+				{
+					cout << "Enter your username: ";
+
+					cin >> username;
+
+					for (int i = 0; i < UserList.size(); i++)
+					{
+						if (username == UserList[i].getUsername())
+						{
+							checkUserExists = true;
+						}
+					}
+
+					if (checkUserExists == false)
+					{
+						User newUser;
+
+						newUser.setUsername(username);
+
+						UserList.push_back(newUser);
+
+						userWorked = true;
+					}
+
+					else
+					{
+						cout << "ERROR: Username is unavailable" << endl;
+					}
+				}
+
+				//Prompt user to set up their password
+				cout << "Enter your password: ";
+
+				string password;
+
+				cin >> password;
+			}
+
+			else
+			{
+
+			}
+		}
+
+		//Displays a list of commands
+		else if (command1 == "help")
+		{
+			cout << "List of available commands: " << endl << "create account - initiates account creation" << endl << "exit - exits program" << endl;
+		}
+
+		//Exits program
+		else if (command1 == "exit")
+		{
+			cout << "Exiting program..." << endl;
+			runProg = false;
+		}
+
+		//Error message for invalid command
+		else
+		{
+			cout << "ERROR: Invalid command" << endl;
+		}
+	}
+}
