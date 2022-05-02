8257bcd79bcce78eb60d63d491d911f3325e92c8
davidaheaton <104536790+davidaheaton@users.noreply.github.com>
2022-05-02 13:45:34 -0500

    changed mistake in setAmountInStock parameter

--- Program Files/Book.h
+++ Program Files/Book.h
@@ -14,7 +14,7 @@ private:
 public:
     Book(int isbn, int amountInStock, float price, string title, string author, string genre) : isbn(isbn), amountInStock(amountInStock) price(price), title(title), author(author), genre(genre) {}
 
-    void setAmountInStock(amountInStock);
+    void setAmountInStock(int amountInStock);
     int getIsbn();
     int getAmountInStock();
     float getPrice();
