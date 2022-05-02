50d918f4ed48b32616bbc5c1c11cac30b21de301
Cassie Javorsky <103222834+clj500@users.noreply.github.com>
2022-05-02 16:57:31 -0500

    Update main.cpp

--- Program Files/main.cpp
+++ Program Files/main.cpp
@@ -1,4 +1,4 @@
-﻿// MethodsProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// MethodsProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -13,7 +13,8 @@ using namespace::std;
 int main()
 {
 	bool runProg = true;
-	string user_input, command1;
+	bool LoggedIn = false;
+	string user_input, command1, userLoggedIn;
 	vector<User> UserList;
 
 	Tokenizer tkn;
@@ -88,6 +89,14 @@ int main()
 				string password;
 
 				cin >> password;
+
+				for (int i = 0; i < UserList.size(); i++)
+				{
+					if (username == UserList[i].getUsername())
+					{
+						UserList[i].setPassword(password);
+					}
+				}
 			}
 
 			else
@@ -105,59 +114,82 @@ int main()
 			string password;
 			bool passwordMatches = false;
 
-			//Check for account input and that account with that username exists
-			if (tkn.readWord(username))
+			if (LoggedIn == false)
 			{
-				if (tkn.readWord(password))
+				//Check for account input and that account with that username exists
+				if (tkn.readWord(username))
 				{
-					for (int i = 0; i < UserList.size(); i++)
+					if (tkn.readWord(password))
 					{
-						if (username == UserList[i].getUsername())
+						for (int i = 0; i < UserList.size(); i++)
 						{
-							checkUserExists = true;
-							cout << UserList[i].getPassword();
-
-							if (password == UserList[i].getPassword())
+							if (username == UserList[i].getUsername())
 							{
-								passwordMatches = true;
+								checkUserExists = true;
+
+								if (password == UserList[i].getPassword())
+								{
+									passwordMatches = true;
+								}
 							}
 						}
 					}
-				}
 
-				//If account does exist, check that password is present and correct
-				if (checkUserExists == true)
-				{
-					if (passwordMatches == true)
+					//If account does exist, check that password is present and correct
+					if (checkUserExists == true)
 					{
-						cout << "Login successful" << endl;
+						if (passwordMatches == true)
+						{
+							cout << "Login successful" << endl;
+							LoggedIn = true;
+							userLoggedIn = username;
+						}
+
+						else
+						{
+							cout << "ERROR: Incorrect password" << endl;
+						}
 					}
 
 					else
 					{
-						cout << "ERROR: Incorrect password" << endl;
+						cout << "ERROR: An account does not exist with this username" << endl;
 					}
 				}
 
 				else
 				{
-					cout << "ERROR: An account does not exist with this username" << endl;
+					cout << "ERROR: Incomplete command" << endl;
 				}
 			}
 
 			else
 			{
-				cout << "ERROR: Incomplete command" << endl;
+				cout << "ERROR: " << userLoggedIn << " is already logged in" << endl;
+			}
+		}
+
+		else if (command1 == "logout")
+		{
+			if (LoggedIn == true)
+			{
+				cout << "Logging out " << userLoggedIn << endl;
+				LoggedIn = false;
+			}
+			else
+			{
+				cout << "ERROR: No account signed in" << endl;
 			}
 		}
 
 		//Displays a list of commands
 		else if (command1 == "help")
 		{
-			cout << "List of available commands: " << endl 
-				<< "create account - initiates account creation" << endl 
+			cout << "List of available commands: " << endl
+				<< "create account - initiates account creation" << endl
 				<< "login <username> <password> - login to an existing account" << endl
-				<< "help - displays a list of valid commands" << endl 
+				<< "logout - logs user out of account" << endl
+				<< "help - displays a list of valid commands" << endl
 				<< "exit - exits program" << endl;
 		}
 
