9caca6ac2475949f96da9e33436bbf5927c4bd25
davidaheaton <104536790+davidaheaton@users.noreply.github.com>
2022-05-03 12:01:29 -0500

    scrap date and time

--- Program Files/History.h
+++ Program Files/History.h
@@ -9,18 +9,14 @@ using namespace::std;
 class History {
 	private:
 		int userId, orderId;
-		string date;
-		string time;
 		float orderTotal;
 	public:
 		vector<Book> orderContent;
 	
-		History() : userId(0), orderId(1), date(""), time(""), orderTotal(0.00) {}
-		History(int userId, int orderId, string date, string time, float orderTotal) : userId(userId), orderId(orderId), date(date), time(time), orderTotal(orderTotal) {}
+		History() : userId(0), orderId(1), orderTotal(0.00) {}
+		History(int userId, int orderId, float orderTotal) : userId(userId), orderId(orderId), orderTotal(orderTotal) {}
 		void displayOrderContent();
 		int getUserId();
 		int getOrderId();
-		string getDate();
-		string getTime();
 		float getOrderTotal();
 };
