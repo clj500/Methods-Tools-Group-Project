bf371cea1669371d0f5877931facaceddead56f0
davidaheaton <104536790+davidaheaton@users.noreply.github.com>
2022-05-03 11:21:17 -0500

    removed Book n:

--- Program Files/History.cpp
+++ Program Files/History.cpp
@@ -3,18 +3,11 @@
 void History::displayOrderContent() {
 
     for (int i = 0; i < orderContent.size(); i++) {
-            cout << "Book" << i + 1 << endl;
             cout << "   Title - " << orderContent[i].getTitle() << endl;
             cout << "    ISBN - " << orderContent[i].getIsbn() << endl;
             cout << "   Price - " << orderContent[i].getPrice() << endl;
-            cout << "Quantity - " << orderContent[i].getQuantity() << endl;
+            cout << "Quantity - " << orderContent[i].getQuantity() << endl << endl;
         }
-            
-
-
-         
-
-
 }
 int History::getUserId() {
     return userId;
