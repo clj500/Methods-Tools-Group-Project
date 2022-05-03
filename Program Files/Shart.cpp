9cd3437e7b56950c65dc3895ca7386738ccdd32c
davidaheaton <104536790+davidaheaton@users.noreply.github.com>
2022-05-02 20:36:18 -0500

    removed cartId and updated some parameter names

--- Program Files/Shart.cpp
+++ Program Files/Shart.cpp
@@ -8,18 +8,17 @@ using namespace std;
 class Cart
 {
 private:
-    int userId;
-    int cartId; 
-    int cartTotal;
+    int userId; 
+    float cartTotal;
     vector<Book> cartContents;
     
 public:
-    void addToCart(Book novel, int quantity);
-    void removeBook(string bookName, int quantity);
+    Cart(int userId) : userId(userId), cartTotal(0) {}
+    void addToCart(Book novel);
+    void removeBook(string bookName);
     void outputCartContents();
     void checkoutCart();
     int getUserId();
-    int getCartId();
     int getCartTotal();
 
 
@@ -28,15 +27,11 @@ int Cart::getUserId()
 {
     return userId;
 }
-int Cart::getCartId()
-{
-    return cartId;
-}
-int Cart::getCartTotal()
+float Cart::getCartTotal()
 {
     return cartTotal;
 }
-void Cart::addToCart(Book novel, int quantity)
+void Cart::addToCart(Book novel)
 {
     cartContents.push_back(novel);
     
